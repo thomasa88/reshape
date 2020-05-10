de0873bd770b1e4842d7b23e0ae7acada6cb2b19
Thomas Axelsson <thomasa88@gmail.com>
2020-05-10 15:33:12 +0200

    Include bookmarklet in index.html with build script

--- build.sh
+++ build.sh
@@ -4,6 +4,11 @@ set -e
 
 VERSION=$1
 
+if [[ -z $VERSION ]]; then
+    echo "Usage: $0 <VERSION>"
+    exit 1
+fi
+
 echo "Version: $VERSION"
 
 mkdir -p output
@@ -13,8 +18,8 @@ mkdir -p output
 # Something breaks --wrap reshape. Results in very short output.
 # Need to protect all properties that are stored in JSON if we want to mangle-props. --mangle-props "reserved=['_data', 'handler', 'keydown']"
 echo -n 'javascript:'
-cat reshape.js | sed "s/<VERSION>/$VERSION/" | node_modules/.bin/uglifyjs --compress --mangle "reserved=['$', 'jQuery']" --rename | head -c -1
-echo 'reshapeInit();void(0)'
+(cat reshape.js | sed "s/<VERSION>/$VERSION/" | node_modules/.bin/uglifyjs --compress --mangle "reserved=['$', 'jQuery']" --rename | head -c -1 && \
+     echo -n 'reshapeInit();void(0)') |  node --eval "process.stdin.on('data', data => { process.stdout.write(encodeURIComponent(data)); })"
 ) > output/reshape-$VERSION-bookmarklet.js
 
 (
@@ -36,4 +41,10 @@ echo 'reshapeInitWhenStable();'
 cp output/reshape-$VERSION.user.js output/reshape-latest.user.js
 
 cp reshape.js docs/reshape-latest.js
-cp output/reshape-$VERSION-bookmarklet.js docs/_includes/reshape-latest-bookmarklet.js
+
+(
+    echo -n '<!-- BOOKMARKLET LINK --><a href="'
+    cat output/reshape-$VERSION-bookmarklet.js
+    echo '">ReShape '$VERSION'</a>'
+) > output/reshape-$VERSION-bookmarklet-link.js
+sed -i -e "/<!-- BOOKMARKLET LINK -->.*/{r output/reshape-$VERSION-bookmarklet-link.js" -e "d}" docs/index.html
